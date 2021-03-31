0632e4fbeb94965f6d31e3f0ddb1789deca08418
121988-dt <81711229+121988-dt@users.noreply.github.com>
2021-03-31 12:23:58 -0700

    Update Mission 1 plot.py
    Mission 1

--- Mission 1 plot.py
+++ Mission 1 plot.py
@@ -25,49 +25,32 @@ ax.plot3D(X,Y,Z,'*-')
 #Show 3d plot
 plt.show()
 
+# Put Tello into command mode
+send("command", 5)
+
+# Send the takeoff command
+send("takeoff", 5)
+send("stop", 5)
 
 #def makePedals ();
-#    curve(64,102,0,0,229,0,60);
-#    curve(-64,-102,0,0,-229,0,60);
+    curve(64,102,0,0,229,0,60);
+    curve(-64,-102,0,0,-229,0,60);
     
-#    curve(64,-102,0,0,-229,0,60);
-#    curve(-64,102,0,0,229,0,60); 
-#    curve(102,64,0,229,0,0,60);
-#    curve(-102,-64,0,-229,0,0,60);
-#    curve(-102,-64,0,-229,0,0,60);
-#    curve(102,64,0,229,0,0,60);
-
-def waypointA():
-    send("stop" + str(5))
-
-def waypointB():
-    send("streamon")
-    send("cw" + str(360),5)
-    send("streamoff")
-
-def waypointC():
-    send("flip" + str(f),5)
-    send("flip" + str(b),5)
-
-def waypointD():
-    radius = 10
-    photo_count = 20
-    angle_increment = 360 + photo_count
-    x1 = radius
-    y1 = 0
-    x2 = cos(angle_increment)*radius
-    y2 = sin(angle_increment)*radius 
-    distance = ((x2-x1)**2+(y2-y1)**2)**(1/2)
-    send("forward" + str(radius),5)
-    send("cw" + str(180),5)
-    time = 1
-    while time < photo_count:
-        send("stop"+ str(1))
-        send("ccw" + str(angle_increment),5)
-        send("right" + str(distance),5)
-        time = time + 1
+    curve(64,-102,0,0,-229,0,60);
+    curve(-64,102,0,0,229,0,60); 
+    curve(102,64,0,229,0,0,60);
+    curve(-102,-64,0,-229,0,0,60);
+    curve(-102,-64,0,-229,0,0,60);
+    curve(102,64,0,229,0,0,60);
 
 
+# Land
+send("land", 5)
 
+# Print message
+print("Mission completed successfully!")
+
+# Close the socket
+sock.close()
 
 
